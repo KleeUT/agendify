f23df2982f057b3cb557499908e9a3ef5c387041
Klee Thomas <klee.ut@gmail.com>
2024-06-22 17:21:48 +1000

    :green_heart: adding moving dynamo client to top level config

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,10 +2,18 @@ import { Config } from "../config";
 import { initialise as initialiseConferenceContext } from "./conference/context";
 import { initialiseSpeakerContext } from "./speaker/context";
 import { initialiseSessionContext } from "./session/context";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export function initialise(config: Config) {
-  const conferenceContext = initialiseConferenceContext(config);
-  const speakerContext = initialiseSpeakerContext(config);
+  const dynamoDocumentClient = DynamoDBDocumentClient.from(
+    new DynamoDBClient(),
+  );
+  const conferenceContext = initialiseConferenceContext(
+    config,
+    dynamoDocumentClient,
+  );
+  const speakerContext = initialiseSpeakerContext(config, dynamoDocumentClient);
   const sessionContext = initialiseSessionContext(config);
   return { ...conferenceContext, ...speakerContext, ...sessionContext };
 }
